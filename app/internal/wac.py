7f3d16b3d0b9e756902bd02f2dab98b0c34107d6
Stijn Tintel <stijn@linux-ipv6.be>
2024-01-10 18:57:39 +0200

    wac: move routes to routers.wac
    And fix formatting with black while at it.

--- app/internal/wac.py
+++ app/internal/wac.py
@@ -1,8 +1,4 @@
-from fastapi import FastAPI, Request, HTTPException
-from fastapi.responses import JSONResponse, PlainTextResponse
 from jsonget import json_get, json_get_default
-from pydantic import BaseModel
-from typing import Optional
 import json
 import logging
 import requests
@@ -207,13 +203,6 @@ def start_typesense():
 
     time.sleep(10)
 
-
-app = FastAPI(title="WAC Proxy",
-              description="Willow Auto Correct REST Proxy",
-              version="0.1",
-              openapi_url="/openapi.json",
-              docs_url="/",
-              redoc_url="/redoc")
 
 # Basic stuff we need
 ha_headers = {
@@ -601,95 +590,3 @@ def api_post_proxy_handler(command, language, distance=SEARCH_DISTANCE, token_ma
     log.info(f"Final speech response '{speech}'")
     log.info(f"Total HA time is {total_ha_time} ms")
     return speech
-
-
-@app.get("/api/add_ha_entities", summary="Add Entities from HA", response_description="Status")
-async def api_add_ha_entities():
-    try:
-        add_ha_entities()
-        return JSONResponse(content={'success': True})
-    except Exception as e:
-        log.exception(f"Add HA Entities failed with {e}")
-        raise HTTPException(
-            status_code=500, detail="WAC Add HA Entities Failed")
-
-
-@app.get("/api/re_init", summary="Wipe DB and Start Over", response_description="Status")
-async def api_reinitialize():
-    try:
-        log.info('Re-initializing...')
-        typesense_client.collections[COLLECTION].delete()
-        init_typesense()
-        return JSONResponse(content={'success': True})
-    except Exception as e:
-        log.exception(f"Re-init failed with {e}")
-        raise HTTPException(status_code=500, detail="WAC Re-init Failed")
-
-
-@app.get("/api/delete", summary="Delete command")
-async def api_delete(id: int):
-    try:
-        log.info(f"Attempting to delete command ID {id}")
-        delete = typesense_client.collections[COLLECTION].documents[id].delete(
-        )
-        command = json_get(delete, "/command")
-        log.info(f"Successfully deleted command '{command}' with id {id}")
-        response = {'success': True, 'deleted': command}
-    except:
-        log.info(f"Failed to deleted command with id {id}")
-        response = {'success': False}
-
-    return JSONResponse(content=response)
-
-
-@app.get("/api/search", summary="WAC Search", response_description="WAC Search")
-async def api_get_wac(command, distance: Optional[str] = SEARCH_DISTANCE, num_results: Optional[str] = CORRECT_ATTEMPTS, exact_match: Optional[bool] = False, semantic: Optional[str] = TYPESENSE_SEMANTIC_MODE, semantic_model: Optional[str] = TYPESENSE_SEMANTIC_MODEL):
-    try:
-        time_start = datetime.now()
-
-        # Little fix for compatibility
-        if semantic == "true":
-            semantic = "on"
-        elif semantic == "false":
-            semantic = "off"
-
-        results = wac_search(command, exact_match=exact_match,
-                             distance=distance, num_results=num_results, raw=True, semantic=semantic, semantic_model=semantic_model)
-
-        time_end = datetime.now()
-        search_time = time_end - time_start
-        search_time_milliseconds = search_time.total_seconds() * 1000
-        log.info('WAC search took ' + str(search_time_milliseconds) + ' ms')
-        return JSONResponse(content=results)
-    except Exception as e:
-        log.exception(f"Search failed with {e}")
-        raise HTTPException(status_code=500, detail="WAC Search Failed")
-
-
-class PostProxyBody(BaseModel):
-    text: Optional[str] = "How many lights are on?"
-    language: Optional[str] = "en"
-
-
-@app.post("/api/proxy", summary="Proxy Willow Requests", response_description="WAC Response")
-async def api_post_proxy(body: PostProxyBody, distance: Optional[int] = SEARCH_DISTANCE, token_match_threshold: Optional[int] = TOKEN_MATCH_THRESHOLD, exact_match: Optional[bool] = False, semantic: Optional[str] = TYPESENSE_SEMANTIC_MODE, vector_distance_threshold: Optional[float] = VECTOR_DISTANCE_THRESHOLD, hybrid_score_threshold: Optional[float] = HYBRID_SCORE_THRESHOLD, semantic_model: Optional[str] = TYPESENSE_SEMANTIC_MODEL, llm_model: Optional[str] = OPENAI_MODEL):
-    try:
-        time_start = datetime.now()
-
-        # Little fix for compatibility
-        if semantic == "true":
-            semantic = "on"
-        elif semantic == "false":
-            semantic = "off"
-
-        response = api_post_proxy_handler(body.text, body.language, distance=distance, token_match_threshold=token_match_threshold,
-                                          exact_match=exact_match, semantic=semantic, semantic_model=semantic_model, vector_distance_threshold=vector_distance_threshold, hybrid_score_threshold=hybrid_score_threshold, llm_model=llm_model)
-        time_end = datetime.now()
-        search_time = time_end - time_start
-        search_time_milliseconds = search_time.total_seconds() * 1000
-        log.info('WAC proxy total time ' +
-                 str(search_time_milliseconds) + ' ms')
-        return PlainTextResponse(content=response)
-    except Exception as e:
-        log.exception(f"Proxy failed with {e}")
-        raise HTTPException(status_code=500, detail="WAC Proxy Failed")
