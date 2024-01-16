5927874a3d3ec0b7541e8c21cbe19ecdaa5bf719
Stijn Tintel <stijn@linux-ipv6.be>
2024-01-16 14:36:47 +0200

    wac: remove proxy route
    This is incompatible with WAS Command Endpoint mode, as we would be
    calling ourselves, which is not supported. Since WAS Command Endpoint
    mode is going to become the default, and non-optional, it doesn't make
    sense to keep the proxy route.

--- app/routers/wac.py
+++ app/routers/wac.py
@@ -4,21 +4,15 @@ from typing import Optional
 
 from jsonget import json_get
 from fastapi import APIRouter, HTTPException
-from fastapi.responses import JSONResponse, PlainTextResponse
-from pydantic import BaseModel
+from fastapi.responses import JSONResponse
 
 from app.internal.wac import (
     COLLECTION,
     CORRECT_ATTEMPTS,
-    HYBRID_SCORE_THRESHOLD,
-    OPENAI_MODEL,
     SEARCH_DISTANCE,
-    TOKEN_MATCH_THRESHOLD,
     TYPESENSE_SEMANTIC_MODE,
     TYPESENSE_SEMANTIC_MODEL,
-    VECTOR_DISTANCE_THRESHOLD,
     add_ha_entities,
-    api_post_proxy_handler,
     init_typesense,
     typesense_client,
     wac_search,
@@ -105,51 +99,3 @@ async def api_get_wac(
     except Exception as e:
         log.exception(f"Search failed with {e}")
         raise HTTPException(status_code=500, detail="WAC Search Failed")
-
-
-class PostProxyBody(BaseModel):
-    text: Optional[str] = "How many lights are on?"
-    language: Optional[str] = "en"
-
-
-@router.post("/proxy", summary="Proxy Willow Requests", response_description="WAC Response")
-async def api_post_proxy(
-    body: PostProxyBody,
-    distance: Optional[int] = SEARCH_DISTANCE,
-    token_match_threshold: Optional[int] = TOKEN_MATCH_THRESHOLD,
-    exact_match: Optional[bool] = False,
-    semantic: Optional[str] = TYPESENSE_SEMANTIC_MODE,
-    vector_distance_threshold: Optional[float] = VECTOR_DISTANCE_THRESHOLD,
-    hybrid_score_threshold: Optional[float] = HYBRID_SCORE_THRESHOLD,
-    semantic_model: Optional[str] = TYPESENSE_SEMANTIC_MODEL,
-    llm_model: Optional[str] = OPENAI_MODEL,
-):
-    try:
-        time_start = datetime.now()
-
-        # Little fix for compatibility
-        if semantic == "true":
-            semantic = "on"
-        elif semantic == "false":
-            semantic = "off"
-
-        response = api_post_proxy_handler(
-            body.text,
-            body.language,
-            distance=distance,
-            token_match_threshold=token_match_threshold,
-            exact_match=exact_match,
-            semantic=semantic,
-            semantic_model=semantic_model,
-            vector_distance_threshold=vector_distance_threshold,
-            hybrid_score_threshold=hybrid_score_threshold,
-            llm_model=llm_model,
-        )
-        time_end = datetime.now()
-        search_time = time_end - time_start
-        search_time_milliseconds = search_time.total_seconds() * 1000
-        log.info("WAC proxy total time " + str(search_time_milliseconds) + " ms")
-        return PlainTextResponse(content=response)
-    except Exception as e:
-        log.exception(f"Proxy failed with {e}")
-        raise HTTPException(status_code=500, detail="WAC Proxy Failed")
